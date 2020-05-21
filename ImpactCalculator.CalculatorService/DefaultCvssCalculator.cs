21cd5a919e6b87a96b52e54f8140196aa9751b2c
domnwern <domn_werner@selinc.com>
2020-05-21 14:55:49 -0700

    Add project files.

---
+++ ImpactCalculator.CalculatorService/DefaultCvssCalculator.cs
@@ -0,0 +1,14 @@
+﻿using Cvss.Net;
+
+namespace ImpactCalculator.CalculatorService
+{
+    public class DefaultCvssCalculator
+    {
+        public double GetScore(string vectorString)
+        {
+            var cvss = new CvssV3(vectorString);
+            var score = cvss.BaseScore;
+            return score;
+        }
+    }
+}
